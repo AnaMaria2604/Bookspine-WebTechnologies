80f2c77051dfcbcb23d22d377a14e1e2b9114394
Loch <ana.26.62.maria.04@gmail.com>
2024-05-29 13:26:38 +0300

    added js doc for adding rating to a book -> not done yet

---
+++ Frontend/giveRating.js
@@ -0,0 +1,44 @@
+function handleStarClick() {
+    let stars = document.querySelectorAll('.stars span')
+    let lastRating = null
+
+    stars.forEach((star) => {
+        star.addEventListener('click', function () {
+            this.setAttribute('data-clicked', 'true')
+
+            let rating = this.getAttribute('starNumber')
+            let productId = this.parentElement.getAttribute('data-productid')
+            lastRating = { stars: rating, 'product-id': productId }
+
+            updateStarColorings(productId, lastRating)
+
+            console.log(
+                'Last Rating Given:',
+                lastRating.stars,
+                'Product ID:',
+                productId
+            )
+            //si aici bagam in baza de date ce e necesar
+        })
+    })
+}
+
+function updateStarColorings(productId, lastRating) {
+    let lastProductStars = document.querySelector(
+        `.stars[data-productid="${productId}"]`
+    )
+    let stars = lastProductStars.querySelectorAll('span')
+
+    stars.forEach((star) => {
+        star.classList.remove('selected')
+    })
+
+    let rating = parseInt(lastRating.stars)
+    stars.forEach((star) => {
+        if (parseInt(star.getAttribute('starNumber')) <= rating) {
+            star.classList.add('selected')
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', handleStarClick)
