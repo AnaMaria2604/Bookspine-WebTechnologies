220ac98c2ec3f93ebbda97282a3a5ddaec1f9d77
Loch <ana.26.62.maria.04@gmail.com>
2024-05-29 12:57:42 +0300

    added folder for front

---
+++ Frontend/Index-Page/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggle = document.getElementById("dropdown-toggle");
+  const dropdownMenu = document.getElementById("dropdown-menu");
+
+  dropdownMenu.style.display = "none";
+
+  dropdownToggle.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (
+      dropdownMenu.style.display === "none" ||
+      dropdownMenu.style.display === ""
+    ) {
+      dropdownMenu.style.display = "block";
+      dropdownMenu.style.position = "absolute";
+      dropdownMenu.style.top = dropdownToggle.offsetTop + "px";
+      dropdownMenu.style.right =
+        dropdownToggle.offsetLeft + dropdownToggle.offsetWidth + "px";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+  });
+});
+
+function setFooterPosition() {
+  const footer = document.querySelector("footer");
+  const windowHeight = window.innerHeight;
+  const bodyHeight = document.body.clientHeight;
+
+  if (windowHeight > bodyHeight) {
+    footer.style.position = "fixed";
+    footer.style.bottom = "0";
+    footer.style.left = "0";
+    footer.style.width = "100%";
+  } else {
+    footer.style.position = "";
+    footer.style.bottom = "";
+    footer.style.left = "";
+    footer.style.width = "";
+  }
+}
+
+window.addEventListener("load", setFooterPosition);
+window.addEventListener("resize", setFooterPosition);
