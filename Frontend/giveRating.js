ba2ee81f19d57fbca210cfc4f93b81a8d8fcfafb
Loch <ana.26.62.maria.04@gmail.com>
2024-06-01 23:49:12 +0300

    updated giveRating

--- Frontend/giveRating.js
+++ Frontend/giveRating.js
@@ -1,76 +1,30 @@
-// function handleStarClick() {
-//     let stars = document.querySelectorAll('.stars span')
-//     let lastRating = null
+document.addEventListener('DOMContentLoaded', () => {
+    let stars = document.getElementsByClassName('star')
 
-//     stars.forEach((star) => {
-//         star.addEventListener('click', function () {
-//             this.setAttribute('data-clicked', 'true')
+    function coloringTheStars(n) {
+        removeColoring()
+        let cls
 
-//             let rating = this.getAttribute('starNumber')
-//             let productId = this.parentElement.getAttribute('data-productid')
-//             lastRating = { stars: rating, 'product-id': productId }
-
-//             updateStarColorings(productId, lastRating)
-
-//             console.log(
-//                 'Last Rating Given:',
-//                 lastRating.stars,
-//                 'Product ID:',
-//                 productId
-//             )
-//             //si aici bagam in baza de date ce e necesar
-//         })
-//     })
-// }
-
-// function updateStarColorings(productId, lastRating) {
-//     let lastProductStars = document.querySelector(
-//         `.stars[data-productid="${productId}"]`
-//     )
-//     let stars = lastProductStars.querySelectorAll('span')
-
-//     stars.forEach((star) => {
-//         star.classList.remove('selected')
-//     })
-
-//     let rating = parseInt(lastRating.stars)
-//     stars.forEach((star) => {
-//         if (parseInt(star.getAttribute('starNumber')) <= rating) {
-//             star.classList.add('selected')
-//         }
-//     })
-// }
-
-// document.addEventListener('DOMContentLoaded', handleStarClick)
-
-function handleStarClick() {
-    let stars = document.querySelectorAll('.stars span')
-
-    stars.forEach((star) => {
-        star.addEventListener('click', function () {
-            let rating = this.getAttribute('starNumber')
-            let productId = this.parentElement.getAttribute('data-productid')
-
-            updateStarColorings(productId, rating)
-
-            console.log('Last Rating Given:', rating, 'Product ID:', productId)
-        })
-    })
-}
-
-function updateStarColorings(productId, newRating) {
-    let stars = document.querySelectorAll(
-        `.stars[data-productid="${productId}"] span`
-    )
-
-    stars.forEach((star) => {
-        let starNumber = parseInt(star.getAttribute('starNumber'))
-        if (starNumber <= newRating) {
-            star.classList.add('selected')
-        } else {
-            star.classList.remove('selected')
+        for (let i = 0; i < n; i++) {
+            if (n == 1) cls = 'one'
+            else if (n == 2) cls = 'two'
+            else if (n == 3) cls = 'three'
+            else if (n == 4) cls = 'four'
+            else if (n == 5) cls = 'five'
+            stars[i].className = 'star ' + cls
         }
-    })
-}
+        console.log('Ratingul dat este: ' + n)
+    }
 
-document.addEventListener('DOMContentLoaded', handleStarClick)
+    function removeColoring() {
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].className = 'star'
+        }
+    }
+
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].addEventListener('click', function () {
+            coloringTheStars(i + 1)
+        })
+    }
+})
