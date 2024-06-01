30628767fba0eb657a2a5cab8254e9b8e60a8d9e
Loch <ana.26.62.maria.04@gmail.com>
2024-06-01 23:49:37 +0300

    updated showRating

--- Frontend/showRating.js
+++ Frontend/showRating.js
@@ -1,18 +1,35 @@
-function showStars(numStars) {
-    let starsContainer = document.querySelector('.avgRating')
+document.addEventListener('DOMContentLoaded', function () {
+    const starContainer = document.getElementById('starContainer')
+    const number = parseInt(starContainer.getAttribute('data-number'))
+    showStars(number)
+})
 
-    if (starsContainer) {
-        starsContainer.innerHTML = ''
+function showStars(number) {
+    if (
+        typeof number !== 'number' ||
+        !Number.isInteger(number) ||
+        number < 1 ||
+        number > 5
+    ) {
+        console.error('Please provide a valid integer between 1 and 5.')
+        return
+    }
 
-        for (let i = 1; i <= numStars; i++) {
-            let star = document.createElement('span')
-            star.textContent = '&#9733'
-            star.setAttribute('starNumber', i)
-            starsContainer.appendChild(star)
-        }
-    } else {
-        console.error('Stars container not found!')
+    let starContainer = document.getElementById('starContainer')
+
+    starContainer.innerHTML = ''
+
+    for (let i = 1; i <= number; i++) {
+        let star = document.createElement('span')
+        star.textContent = '★'
+        star.classList.add('gold-star')
+        starContainer.appendChild(star)
+    }
+
+    for (let i = number + 1; i <= 5; i++) {
+        let star = document.createElement('span')
+        star.textContent = '★'
+        star.classList.add('white-star')
+        starContainer.appendChild(star)
     }
 }
-
-showStars(5)
