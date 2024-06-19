43d6c90fcff8065eff7cc1da9815db2fc1d493ab
Loch <ana.26.62.maria.04@gmail.com>
2024-06-19 17:12:23 +0300

    changes

---
+++ Backend/reviewBook.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+console.log('reviewbook');
+
+const handleReviewDetailsRequest = (req, res) => {
+    const filePath = path.join(__dirname, '../Frontend/Review-Page/review.html')
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404)
+            res.end('404 Not Found')
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        }
+    })
+}
+
+module.exports = {
+    handleReviewDetailsRequest,
+}
---
+++ Backend/reviewService.js
@@ -0,0 +1,23 @@
+console.log('reviewservice')
+
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParts = window.location.pathname.split('/')
+    const bookId = urlParts.pop() || urlParts.pop()
+
+    fetch(`/review-book/${bookId}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const details = document.getElementById('book-read')
+            data.forEach((book) => {
+                const bookElement = document.createElement('div')
+                bookElement.innerHTML = `
+                <div>
+                    <a href="/review-book/${bookId}">Read</a>
+                </div>`
+                details.appendChild(bookElement)
+            })
+        })
+        .catch((error) => {
+            console.error('Error fetching review page:', error)
+        })
+})
